300bfcbcd02fcffdf8d0534360bfeef0d8942773
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2026-04-03 21:36:20 -0300

    remove: delete api.php routes file

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\MetricsSnapshotController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('api.key')->group(function () {
-    Route::get('/metrics/snapshot', MetricsSnapshotController::class);
-});
